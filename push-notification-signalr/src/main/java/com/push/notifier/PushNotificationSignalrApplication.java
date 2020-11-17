83ecd3744f29f3e7e41c034ac9f4ad8eca2526a0
ShrutiKamboj <shrutikamboj@gmail.com>
2020-11-17 22:26:57 +0530

    Create PushNotificationSignalrApplication.java
    Added project base

---
+++ push-notification-signalr/src/main/java/com/push/notifier/PushNotificationSignalrApplication.java
@@ -0,0 +1,13 @@
+package com.push.notifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PushNotificationSignalrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PushNotificationSignalrApplication.class, args);
+	}
+
+}
