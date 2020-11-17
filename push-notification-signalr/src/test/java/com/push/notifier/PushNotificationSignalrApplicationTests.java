06368fbfffd5799ba391956ac7bb86070cd2a44d
ShrutiKamboj <shrutikamboj@gmail.com>
2020-11-17 22:27:17 +0530

    Create PushNotificationSignalrApplicationTests.java
    Added project base

---
+++ push-notification-signalr/src/test/java/com/push/notifier/PushNotificationSignalrApplicationTests.java
@@ -0,0 +1,12 @@
+package com.push.notifier;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotificationSignalrApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
